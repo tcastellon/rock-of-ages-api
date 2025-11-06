10c231a7530caa7b263edff6f3e84e7a0d3a160d
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-06 11:40:46 -0600

    updated

--- rockapi/views/type_view.py
+++ rockapi/views/type_view.py
@@ -39,4 +39,4 @@ class TypeSerializer(serializers.ModelSerializer):
     """JSON serializer for types"""
     class Meta:
         model = Type
-        fields = ('id', 'label', )
+        fields = ( 'id', 'label', )
