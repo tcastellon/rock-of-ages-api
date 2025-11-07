0e5890b0c26d8d6777adbb7cc0efd3ab894d3e83
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-06 23:11:02 -0600

    added destroy to delete rocks and updated list to also retrieve a list of rocks by user

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -36,12 +36,37 @@ class RockView(ViewSet):
         Returns:
             Response -- JSON serialized array
         """
+        owner_only = self.request.query_params.get("owner", None)
+
         try:
             rocks = Rock.objects.all()
+            if owner_only is not None and owner_only == "current":
+                rocks = rocks.filter(user=request.auth.user)
+
             serializer = RockSerializer(rocks, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            rock = Rock.objects.get(pk=pk)
+            if rock.user.id == request.auth.user.id:
+                rock.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': 'You do not own that rock'}, status=status.HTTP_403_FORBIDDEN)
+
+        except Rock.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class RockOwnerSerializer(serializers.ModelSerializer):
     """JSON serializer"""
